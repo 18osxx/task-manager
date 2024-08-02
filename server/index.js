b0bb2fb222036ed80f11ada45d9d5a5cbfaa7c24
Tommy <tj.barden24@icloud.com>
2024-08-02 10:45:55 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const userRoutes = require('../client/src/routes/userRoutes');
-const taskRoutes = require('../client/src/routes/taskRoutes');
-require('dotenv').config({ path: './.env' });
-
-const mongodbUri = process.env.MONGODB_URI;
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,13 +11,20 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(mongodbUri).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((error) => {
-  console.error('Error connecting to MongoDB:', error.message);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Task Manager API");
 });
 
-app.use('/users', userRoutes);
-app.use('/tasks', taskRoutes);
+app.use("/users", userRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
