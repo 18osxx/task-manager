cbdf95db7f3e138143cd16d696a290fcd61b4dd5
Tommy <tj.barden24@icloud.com>
2024-08-02 09:06:46 +0100

    Setup task database model

---
+++ server/models/Task.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose'); // Import mongoose
+
+const TaskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    dueDate: {
+        type: Date,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'In Progress', 'Completed'],
+        default: 'Pending',
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+});
