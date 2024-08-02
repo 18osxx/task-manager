b0a10aa5f9fe28237ac14c19ead34838acb6706c
Tommy <tj.barden24@icloud.com>
2024-08-02 09:59:59 +0100

    Update imports, and formatted code

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const taskRoutes = require("./routes/taskRoutes");
-const dotenv = require("dotenv");
+require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,12 +12,19 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
 
 app.use("/users", userRoutes);
 app.use("/tasks", taskRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
