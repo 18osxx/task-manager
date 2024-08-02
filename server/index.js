d4e7ed617cc18be4660752fe54a42b7755fd5b08
Tommy <tj.barden24@icloud.com>
2024-08-02 10:12:55 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const userRoutes = require("./routes/userRoutes");
-const taskRoutes = require("./routes/taskRoutes");
-require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+require('dotenv').config({ path: './.env' });
+
+const mongodbUri = process.env.MONGODB_URI;
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,19 +14,13 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error.message);
-  });
+mongoose.connect(mongodbUri).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error.message);
+});
 
-app.use("/users", userRoutes);
-app.use("/tasks", taskRoutes);
+app.use('/users', userRoutes);
+app.use('/tasks', taskRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
