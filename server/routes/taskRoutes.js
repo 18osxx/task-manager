b22be3fb9782add5f3ac287ba865c41226c59689
Tommy <tj.barden24@icloud.com>
2024-08-02 09:59:44 +0100

    Change from .get to .post

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -3,7 +3,7 @@ const { createTask, getTasks, updateTask, deleteTask } = require('../controllers
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
-router.get('/', authMiddleware, createTask);
+router.post('/', authMiddleware, createTask);
 router.get('/', authMiddleware, getTasks);
 router.put('/:id', authMiddleware, updateTask);
 router.delete('/:id', authMiddleware, deleteTask);
