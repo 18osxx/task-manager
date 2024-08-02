bd98fac7fe284c372cd1f988b92bddf828c48c95
Tommy <tj.barden24@icloud.com>
2024-08-02 10:20:00 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,31 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import TaskList from "./components/TaskList";
 import AuthProvider from "./context/AuthContext";
+import "./styles.css";
+
+const HomePage = () => (
+  <div className="homepage">
+    <h1>Welcome to Task Manager</h1>
+    <p>Your personal task management solution</p>
+    <button onClick={() => (window.location.href = "/register")}>
+      Get Started
+    </button>
+  </div>
+);
 
 const App = () => {
   return (
     <AuthProvider>
       <Router>
-        <Switch>
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
-          <Route path="/tasks" component={TaskList} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/tasks" element={<TaskList />} />
+        </Routes>
       </Router>
     </AuthProvider>
   );
