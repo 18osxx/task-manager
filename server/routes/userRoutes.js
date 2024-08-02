863d45dc3c6be94b8e84947e4f2e2b19874f4f8a
Tommy <tj.barden24@icloud.com>
2024-08-02 09:59:26 +0100

    Corrected code

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,59 +1,8 @@
-// server/controllers/userController.js
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const express = require('express');
+const { registerUser, loginUser } = require('../controllers/userController');
+const router = express.Router();
 
-const registerUser = async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const existingUser = await User.findOne({ username });
-    if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
-    }
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
-    const hashedPassword = await bcrypt.hash(password, 12);
-
-    const newUser = new User({ username, password: hashedPassword });
-    await newUser.save();
-
-    const token = jwt.sign(
-      { username: newUser.username, id: newUser._id },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
-
-    res.status(201).json({ result: newUser, token });
-  } catch (error) {
-    res.status(500).json({ message: "Something went wrong" });
-  }
-};
-
-const loginUser = async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const existingUser = await User.findOne({ username });
-    if (!existingUser) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    const isPasswordCorrect = await bcrypt.compare(
-      password,
-      existingUser.password
-    );
-    if (!isPasswordCorrect) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
-
-    const token = jwt.sign(
-      { username: existingUser.username, id: existingUser._id },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
-
-    res.status(200).json({ result: existingUser, token });
-  } catch (error) {
-    res.status(500).json({ message: "Something went wrong" });
-  }
-};
-
-module.exports = { registerUser, loginUser };
+module.exports = router;
